6ef82720d08f190dffdf1365e34d4e9786432426
fedorov.artiom@gmail.com <fedorov.artiom@gmail.com@28a3e5da-229f-01ad-89ca-73bb15142725>
2014-03-15 15:53:05 +0000

    Relever reelement les mails, les supprime puis les rajoute a la base et effectue systeme de vote

--- niko-niko/controllers/calendars/add.php
+++ niko-niko/controllers/calendars/add.php
@@ -1,46 +1,44 @@
 <?php
 
 	$mailbox = new MyMail();
-	$msgs = $mailbox->removeOne();
-	
-	//exit();
-	//print_r($msgs = $mailbox->getNew());
 
 	$ht = "";
-	$msgs = $mailbox->getNew();
+	$msgs = $mailbox->getAllAndRemove(true);
+	
 	foreach($msgs as $m) {
 		
-		//$m['message']
-		//	echo($m['subject']);
-		//MySmiley::str2smiley($m['subject'], true);
 		if(MySmiley::str2smiley($m['subject'], false)) {
 
 			$ht .= MySmiley::str2smiley($m['subject'], false) ;
-			
+			//$m['message']
 		}
-		$m['date'] = $m['date'];
+		//$m['date'] = $m['date'];
+		$m['time'] = strtotime($m['date']);
+		$m['ftime'] = date("Y-m-y", $m['time']);                           // 03.10.01
 		print_r($m);
 		
+		
+		$sujet = $m['subject'];
+		$user = $m['from'];
+		$date = $m['ftime'];
+		
+		
+		if (MySmiley::detect($sujet)) {
+			if(users::checkUserCanAdd($user, $date)) {	
+				$smiley = array();
+				$smiley['smileycode'] = MySmiley::detect($sujet);
+				$smiley['created'] = $date;
+				$smiley['inteams'] = users::getTeamIds($user);
+				$item['smiles'] = $smiley;
+			
+				OrmSmiley::addSmile($item);
+				users::addVoteDay($user, $date);
+			}
+		}
+		
+			
+		
 	
-	}
-
-
-	//$date = "2014-03-01";
-	$date = "2014-03-02";
-	$user = "adel.lynda@gmail.com";
-	//$user = "fedorov.artiom@gmail.com";
-
-
-	$smiley['smileycode'] = 20;
-	$smiley['created'] = $date;
-	$smiley['inteams'] = users::getTeamIds($user);
-	$item['smiles'] = $smiley;
-
-	if(users::checkUserCanAdd($user, $date)) {	
-		
-		OrmSmiley::addSmile($item);
-		users::addVoteDay($user, $date);
-		
 	}
 
 ?>
