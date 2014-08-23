ca09ee2c6294a74025f38dfdf98c04cf94851516
fedorov.artiom <fedorov.artiom@28a3e5da-229f-01ad-89ca-73bb15142725>
2014-08-23 14:49:30 +0000

    Ajout du dossier de conf avec les fichiers exemple

---
+++ niko-niko/config/db.sample.php
@@ -0,0 +1,9 @@
+<?
+
+	define('DB_HOST', "..."); // replace ... by your host
+	define('DB_USER', "..."); // replace ... by your user
+	define('DB_PASSWORD', "..."); // replace ... by your user
+	define('DB_NAME', "...");
+	
+	
+
---
+++ niko-niko/config/mail.sample.php
@@ -0,0 +1,8 @@
+<?
+
+	define('MAIL_HOST', '{imap.gmail.com:993/imap/ssl}INBOX'); // replace ... by your host
+	
+	define('MAIL_USER', 'yourgmailadress@gmail.com'); // replace ... by your user
+	define('MAIL_PASSWORD', 'gmailpassword'); // replace ... by your user
+	
+
