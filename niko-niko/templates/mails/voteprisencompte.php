30a7a55f32ac09e4fe66ae8569b205daca34a16b
fedorov.artiom <fedorov.artiom@28a3e5da-229f-01ad-89ca-73bb15142725>
2014-09-02 23:36:40 +0000

    Pas de repetition du smiley dans la confirmation pour + d'anonymie

--- niko-niko/templates/mails/voteprisencompte.php
+++ niko-niko/templates/mails/voteprisencompte.php
@@ -1,4 +1,4 @@
 <p>Merci d'avoir envoy&eacute; votre smiley,<br />
-Ce dernier <?=$md['smiley'];?> &eacute; bien &eacute;t&eacute; pris en compte<br />
+<? //$md['smiley'];?>Ce dernier &eacute; bien &eacute;t&eacute; pris en compte<br />
 <br />
 <b><a href="<?=FRONT_URL?>">Niko-Niko</a></b></p>
