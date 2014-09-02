0673c48d66c695adb063fa71e079e1f59f0dacaa
fedorov.artiom <fedorov.artiom@28a3e5da-229f-01ad-89ca-73bb15142725>
2014-09-02 23:27:40 +0000

    Pas de lien du niko niko si dans aucune equipe

--- niko-niko/templates/mails/noniko.php
+++ niko-niko/templates/mails/noniko.php
@@ -2,5 +2,5 @@
 <br>
 Rapprochez vous de l'administrateur du niko-niko<br>
 <br>
-<b><a href="<?=FRONT_URL?>">Niko-Niko</a></b>
+<b><a href="http://artiom-fedorov.blogspot.fr">A propos du Niko-Niko</a></b>
 </p>
