b7c3d27e361e3134a501efa4651632863455e848
fedorov.artiom <fedorov.artiom@28a3e5da-229f-01ad-89ca-73bb15142725>
2015-01-25 18:52:25 +0000

    Nettoyage de code

--- niko-niko/libs/smiley.class.php
+++ niko-niko/libs/smiley.class.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ *	Classe servant a travailler avec les emoticones (smileys)
+ *
+ *	@brief Premet l'affichage la reconnaissance des emoticones	
+ *	@decription Definit les scores
+ *	@author	Artiom FEDOROV
+ *
  * codes:
  *  30 = :-)
  *  20 = :-|
@@ -11,20 +17,34 @@
 
 class MySmiley {
 
+	// Chemin vers les images emoticones
 	private static $imgpath = "./images/";
 
 
+	/**
+	 *	Methode de conversion string en smileys
+	 *
+	 *	@param str	prend en parametre une chaine avec smiley dedan a detecter
+	 *	@param echo	parametre definissant affichage immediat ou renvoi chaine de carracteres
+	 *	@return string	Renvoi la chaine de carracteres html du smiley detecté
+	 */
+	 
 	public static function str2smiley($str, $echo = false) {
-	
 		return self::getHtmlSmile(self::detect($str), $echo); 
-
 	}
 	
 
-
-
-
-	// La méthode qui se fait pas chier!	
+	/**
+	 *	Methode simpliste de detection de smiley
+	 *
+	 *	@brief	renvoi le score du smiley detecté dans une chaine de carracteres
+	 *	@description	Si dans une chaine de carracteres :-) ou :-| ou :-( est detecté
+	 *					alors on renvoi le score correspondant
+	 *
+	 *	@return	int 	Renvoi le code du smiley trouvé dans la chaine
+	 *	@param String Prend en parametre la chaine de carracteres
+	 */
+	
 	public static function detect($str) {
 	
 		if (strpos($str, ':-)') !== false) {
@@ -35,22 +55,24 @@ class MySmiley {
 			return 20;
 		}
 		
-		
 		if (strpos($str, ':-(') !== false) {
 			return 10;
 		}
-		
-		return false;
-	
+		return false;	
 	}
 
-	
 
+	/**
+	 *	Methode permettant d'obtenir l'html a partir du smiley code
+	 *	@param	int		Prend en parametre le code du smiley : 10/20/30
+	 *	@brief	Conveti un smiley code en html
+	 *	@return String	Renvoi la l'image en html correspondant au score
+	 *
+	 */
+	 
 	public static function getHtmlSmile($smileCode, $echo) {
-	
 		$prefix = "smiley-";
 		$ext = ".png";
-	
 		switch ($smileCode) {
 			case 10:
 				$out = self::htmlImgWrapp(self::$imgpath . $prefix . "10" . $ext);
@@ -64,50 +86,41 @@ class MySmiley {
 			default:
 				$out =  "";
 		}
-		
 		if ($echo) {
 			echo($out);
 		}
-	
 		return $out;
 	}
 
+
+	/**
+	 *	Methode permettant d'obtenir la moyenne
+	 *
+	 */
 
 	public static function getRoundedScore($score, $values = array(0, 10, 20, 30)) {
 	
 		$lessDelta = 100;
 		$pointer = 0;
-		
 		foreach($values as $key=>$val) {
 			if ((($score - $val) * ($score - $val)) < $lessDelta) {
 				$lessDelta = (($score - $val) * ($score - $val));
 				$pointer = $key;
-			
 			}
-		}
-	
+		}	
 		return $values[$pointer];
-	
-	
 	}
 
 
-
+	/**
+	 *	Methode de wrapping pour afficher l'image
+	 *	@brief	html helper 
+	 *
+	 */
 
 	private static function htmlImgWrapp($filename) {
-	
 		$result = '<img width="72" border="0" height="72" src="'.$filename.'" alt="" />';
-		
 		return $result;
 	}
-
-
-
-
 }
-
-
-
-
-
 
